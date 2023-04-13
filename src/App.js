36ffff24b832e816f84371bcec4ee28b63c356ca
“[Amri Iqra Samudra Al-Fatihah <amriiqosamudraalfatihah@gmail.com>
2023-04-13 20:25:45 +0700

    fix bug add data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import './App.css';
-import Admin from './pages/admin';
-import Kasir from './pages/kasir';
-import Login from './pages/login';
-import { Route, Routes } from 'react-router-dom';
-import AddData from './pages/addData';
-import UpdateData from './pages/editData';
-
+import "./App.css";
+import Admin from "./pages/admin";
+import Kasir from "./pages/kasir";
+import { Route, Routes } from "react-router-dom";
+import AddData from "./pages/addData";
+import UpdateData from "./pages/editData";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Kasir/>}/>
-        <Route path='/admin' element={<Admin/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/add' element={<AddData/>}/>
-        <Route path='/updatedata/:id' element={<UpdateData/>}/>
+        <Route path="/" element={<Kasir />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/add" element={<AddData />} />
+        <Route path="/updatedata/:id" element={<UpdateData />} />
       </Routes>
     </div>
   );
